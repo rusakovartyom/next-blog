d8c058040f86521be011a0cf8ef58bce73d82ebb
Artyom Rusakov <popsninka@gmail.com>
2022-11-22 18:17:37 +0300

    Add logic behind showing/hiding Loader

--- components/Loader/Loader.js
+++ components/Loader/Loader.js
@@ -1,4 +1,6 @@
-const Loader = () => {
-  return <div>Loader</div>;
+import styles from './styles.module.css';
+
+const Loader = ({ show }) => {
+  return show ? <div className={styles.loader}></div> : null;
 };
 export default Loader;
