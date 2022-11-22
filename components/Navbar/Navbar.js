d04b803433a3e2b0da3bfd99f76b9972f3409158
Artyom Rusakov <popsninka@gmail.com>
2022-11-23 01:54:12 +0300

    Add conditional content for Navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,4 +1,52 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import clsx from 'clsx';
+
+import styles from './styles.module.css';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const user = null;
+  const username = null;
+  const linkBlue = clsx(styles.navbarLink, styles.navbarLinkBlue);
+
+  return (
+    <nav className={styles.navbar}>
+      <ul className={styles.navbarList}>
+        <li>
+          <Link className={styles.logo} href="/">
+            Blog
+          </Link>
+        </li>
+        {/* {user is signed-in and has username} */}
+        {username && (
+          <>
+            <li className={styles.navbarListLeft}>
+              <Link className={linkBlue} href="/admin">
+                Write Posts
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <Image
+                  className={styles.navbarImg}
+                  src={user?.photoURL}
+                  alt=""
+                />
+              </Link>
+            </li>
+          </>
+        )}
+
+        {/* user is not signed OR has not created username */}
+        {!username && (
+          <li>
+            <Link className={linkBlue} href="/enter">
+              Log in
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
 };
 export default Navbar;
