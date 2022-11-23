4e9387558327a622e15e5d06595f1af974fdaa3c
Artyom Rusakov <popsninka@gmail.com>
2022-11-23 11:47:06 +0300

    Add Toaster to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Navbar from '../components/Navbar/Navbar';
+import { Toaster } from 'react-hot-toast';
 
 import '../styles/globals.css';
 
@@ -7,6 +8,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Toaster />
     </>
   );
 }
