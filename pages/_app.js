6a3c5aeac4279182ba4a73e1ce69eb317704c37e
Artyom Rusakov <popsninka@gmail.com>
2022-11-23 16:05:16 +0300

    Feature/NB-6-auth-context (#8)
    * Add google auth provider
    
    * Add dummy data about user
    
    * Add sign in/out buttons and username form subcomponents
    
    * Move enter.js to separate folder, rename to index.js
    
    * Delete redundant styles for Navbar
    
    * Add styles for Sign in page
    
    * Add Button component and export
    
    * Add google SVG icon to assets
    
    * Add styles for Button component
    
    * Add additional prop for clicks to Button component
    
    * Add onClick logic to SignInButton and logic for SignOutButton
    
    * Add context API for user and username
    
    * Change empty tag on UserContext.Provider
    
    * Provide dummy data
    
    * Add default export
    
    * Connect to user context
    
    * Fix import for UserContext
    
    * Fix import for UserContext
    
    * Add UserContext to sign in page

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,16 @@
 import Navbar from '../components/Navbar/Navbar';
 import { Toaster } from 'react-hot-toast';
+import UserContext from '../lib/context';
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <UserContext.Provider value={{ user: {}, username: 'rusakov' }}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </UserContext.Provider>
   );
 }
 
