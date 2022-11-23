387992ddd386ae42427666f13f25585bd91621c2
Artyom Rusakov <popsninka@gmail.com>
2022-11-23 15:50:49 +0300

    Fix import for UserContext

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import Navbar from '../components/Navbar/Navbar';
 import { Toaster } from 'react-hot-toast';
-import { UserContext } from '../lib/context';
+import UserContext from '../lib/context';
 
 import '../styles/globals.css';
 
