fbf59aa0164977a0b37c52b53b46cfe14b7802d5
Artyom Rusakov <popsninka@gmail.com>
2022-11-23 12:52:29 +0300

    Add Button component and export

---
+++ components/Button/Button.js
@@ -0,0 +1,14 @@
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+const Button = (props) => {
+  const button = clsx(
+    styles.button,
+    props.blue && styles.blue,
+    props.red && styles.red,
+    props.green && styles.green,
+    props.google && styles.google
+  );
+  return <button className={button}>{props.children}</button>;
+};
+export default Button;
