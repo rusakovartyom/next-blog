7cad300a7a2dbb9dcb0d52f17e47ff7adf4dcc4a
Artyom Rusakov <popsninka@gmail.com>
2022-11-23 15:51:02 +0300

    Fix import for UserContext

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import clsx from 'clsx';
 import { useContext } from 'react';
-import { UserContext } from '../../lib/context';
+import UserContext from '../../lib/context';
 
 import styles from './styles.module.css';
 
