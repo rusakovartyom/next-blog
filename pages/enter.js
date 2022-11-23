86b8c3c6f42fde89510ff426c86b722a86d34dc7
Artyom Rusakov <popsninka@gmail.com>
2022-11-23 15:58:08 +0300

    Feature/NB-5-auth (#7)
    * Add google auth provider
    
    * Add dummy data about user
    
    * Add sign in/out buttons and username form subcomponents
    
    * Move enter.js to separate folder, rename to index.js
    
    * Delete redundant styles for Navbar
    
    * Add styles for Sign in page
    
    * Add Button component and export
    
    * Add google SVG icon to assets
    
    * Add styles for Button component
    
    * Add additional prop for clicks to Button component
    
    * Add onClick logic to SignInButton and logic for SignOutButton

--- pages/enter.js
+++
@@ -1,8 +0,0 @@
-const EnterPage = () => {
-  return (
-    <main>
-      <h1>Sign Up</h1>
-    </main>
-  );
-};
-export default EnterPage;
---
+++ pages/enter/index.js
@@ -0,0 +1,56 @@
+import Image from 'next/image';
+import { auth, googleAuthProvider } from '../../lib/firebase';
+import Button from '../../components/Button';
+
+import styles from './styles.module.css';
+
+const EnterPage = () => {
+  const user = null;
+  const username = null;
+
+  return (
+    <main>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
+    </main>
+  );
+};
+export default EnterPage;
+
+const SignInButton = () => {
+  const signInGoogle = async () => {
+    try {
+      await auth.signInWithPopup(googleAuthProvider);
+    } catch (error) {
+      alert(error);
+    }
+  };
+  return (
+    <div className={styles.container}>
+      <Button google onClick={signInGoogle}>
+        <Image
+          className={styles.googleIcon}
+          width="30"
+          height="30"
+          src="/assets/icons8-google.svg"
+          alt=""
+        />
+        Sign in with Google
+      </Button>
+      <Button onClick={() => auth.signInAnonymously()}>
+        Sign in Anonymously
+      </Button>
+    </div>
+  );
+};
+const SignOutButton = () => {
+  return <Button onClick={() => auth.signOut()}>Sign Out</Button>;
+};
+const UsernameForm = () => {};
