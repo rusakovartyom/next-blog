5b57280a76e6b82508cff82ada265bc4dfa95d21
Artyom Rusakov <popsninka@gmail.com>
2022-11-23 17:24:58 +0300

    Add useState, useEffect, useAuthState for hook

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import Navbar from '../components/Navbar/Navbar';
 import { Toaster } from 'react-hot-toast';
 import UserContext from '../lib/context';
@@ -5,6 +7,9 @@ import UserContext from '../lib/context';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  const [user] = useAuthState(auth);
+  const [username, setUsername] = useState(null);
+
   return (
     <UserContext.Provider value={{ user: {}, username: 'rusakov' }}>
       <Navbar />
