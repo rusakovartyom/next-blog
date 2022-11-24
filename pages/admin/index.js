5e049a7ad385bbb8c36bf07c1d7f918d6a36028f
Artyom Rusakov <popsninka@gmail.com>
2022-11-25 02:09:11 +0300

    Add imports and subcomponents

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,10 +1,28 @@
+import { useState, useContext } from 'react';
+import { UserContext } from '../../lib/context';
+import { useRouter } from 'next/router';
+import { firestore, auth, serverTimestamp } from '../../lib/firebase';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import kebabCase from 'lodash.kebabcase';
+import toast from 'react-hot-toast';
+
 import AuthCheck from '../../components/AuthCheck';
+import PostFeed from '../../components/PostFeed';
+
+import styles from './styles.module.css';
 
 const AdminPostsPage = () => {
   return (
     <main>
-      <AuthCheck></AuthCheck>
+      <AuthCheck>
+        <PostList />
+        <CreateNewPost />
+      </AuthCheck>
     </main>
   );
 };
+
+const PostList = () => {};
+const CreateNewPost = () => {};
+
 export default AdminPostsPage;
