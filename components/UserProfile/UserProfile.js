ee962a9dabbd82366022fed51aede0f402ed2de1
Artyom Rusakov <popsninka@gmail.com>
2022-11-24 13:40:19 +0300

    Add markup for UserProfile

--- components/UserProfile/UserProfile.js
+++ components/UserProfile/UserProfile.js
@@ -1,4 +1,22 @@
-const UserProfile = () => {
-  return <div>UserProfile</div>;
+import UserProfilePic from '../UserProfilePic';
+
+import styles from './styles.module.css';
+
+const UserProfile = (user) => {
+  return (
+    <div className={styles.userProfile}>
+      <UserProfilePic
+        className={styles.userImage}
+        src={user.photoURL}
+        alt={user.username}
+        width="20%"
+        height="auto"
+      />
+      <p>
+        <i>@{user.username}</i>
+      </p>
+      <h1>{user.displayName}</h1>
+    </div>
+  );
 };
 export default UserProfile;
