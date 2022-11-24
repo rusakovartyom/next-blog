f8f2997423701c30801b1ad29a01fc004b96a20c
Artyom Rusakov <popsninka@gmail.com>
2022-11-24 22:44:51 +0300

    Add function that retrieves props from the server

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,7 +1,34 @@
+import { getUserWithUsername, postToJSON } from '../../lib/firebase';
+
 import UserProfile from '../../components/UserProfile';
 import PostFeed from '../../components/PostFeed';
 
-const UserProfilePage = ({ user }) => {
+export async function getServerSideProps({ query }) {
+  const { username } = query;
+
+  const userDoc = await getUserWithUsername(username);
+
+  // JSON serializable data
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsQuery = userDoc.ref
+      .collection('posts')
+      .where('published', '==', true)
+      .orderBy('createdAt', 'desc')
+      .limit(5);
+
+    posts = (await postsQuery.get()).docs.map(postToJSON);
+  }
+
+  return {
+    props: { user, posts }, // Will be passed to the page component as props
+  };
+}
+
+const UserProfilePage = ({ user, posts }) => {
   return (
     <main>
       <UserProfile user={user} />
