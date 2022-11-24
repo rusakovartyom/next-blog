8f7becc118afcb55dcbe137f79b899e8638bc9a5
Artyom Rusakov <popsninka@gmail.com>
2022-11-24 23:13:14 +0300

    Add server-side props function

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
+import PostFeed from '../components/PostFeed';
+import Loader from '../components/Loader';
 import styles from '../styles/Home.module.css';
 
+// Max post to query per page
+const LIMIT = 2;
+
+export async function getServerSideProps(context) {
+  const postsQuery = firestore
+    .collectionGroup('posts')
+    .where('published', '==', true)
+    .orderBy('createdAt', 'desc')
+    .limit(LIMIT);
+
+  const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+  return {
+    props: { posts }, // will be passed to the page component as props
+  };
+}
+
 export default function Home() {
-  return <div className={styles.container}></div>;
+  return <main></main>;
 }
