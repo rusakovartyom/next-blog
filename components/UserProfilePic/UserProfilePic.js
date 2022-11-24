2c7f1299ab70fb8150f3dd5f9c6ec3d9d0e6798f
Artyom Rusakov <popsninka@gmail.com>
2022-11-24 22:59:31 +0300

    Feature/NB-9-user-profile-page (#13)
    * Make user profile picture separate component
    
    * Move styles for UserProfilePic component
    
    * Import UserProfilePic component
    
    * Fix props
    
    * Edit alt attribute for Image
    
    * Add UserProfile component
    
    * Add styles for UserProfile component
    
    * Import UserProfile to user profile page
    
    * Add markup for UserProfile
    
    * Add styles for UserProfile
    
    * Import UserProfile to user profile page
    
    * Add PostFeed component and export
    
    * Add styles for PostFeed
    
    * Import PostFeed to user profile page
    
    * Add helper function to fetch user document based on username
    
    * Fix typos
    
    * Remove console.log
    
    * Add function that retrieves props from the server
    
    * Add function that converts doc data to JSON
    
    * Fix/NB-10-user-profile-pic (#12)
    
    * Replace SVG icon to png icon for next/image
    
    * Add container, rework style for Image component
    
    * Edit style for user page profile pic
    
    * Edit styles for navbar profile pic
    
    * Add CSS class for UserProfilePic container
    
    * Remove redundant class, add margin top

--- components/UserProfilePic/UserProfilePic.js
+++ components/UserProfilePic/UserProfilePic.js
@@ -4,13 +4,15 @@ import styles from './styles.module.css';
 
 const UserProfilePic = (props) => {
   return (
-    <Image
-      className={styles.image}
-      src={props.src === null ? '/assets/profile-picture.svg' : props.src}
-      alt={`${props.alt} profile picture`}
-      width={props.width}
-      height={props.height}
-    />
+    <div style={props.style} className={styles.container}>
+      <Image
+        className={styles.image}
+        src={props.src === null ? '/assets/profile-picture.png' : props.src}
+        alt={`${props.alt} profile picture`}
+        fill
+        style={{ objectFit: 'contain' }}
+      />
+    </div>
   );
 };
 
