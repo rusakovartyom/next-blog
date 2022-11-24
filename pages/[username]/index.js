2b08f7ca64700b7cf6317744f44bbccc1af03456
Artyom Rusakov <popsninka@gmail.com>
2022-11-24 14:22:04 +0300

    Import PostFeed to user profile page

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,9 +1,11 @@
 import UserProfile from '../../components/UserProfile';
+import PostFeed from '../../components/PostFeed';
 
 const UserProfilePage = ({ user }) => {
   return (
     <main>
       <UserProfile user={user} />
+      <PostFeed posts={posts} />
     </main>
   );
 };
