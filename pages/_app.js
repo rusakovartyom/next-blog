271f0173737d2276ca9bcbdce180191aac194d18
Artyom Rusakov <popsninka@gmail.com>
2022-11-28 02:11:21 +0300

    Add Head to _app.js with favicons

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Navbar from '../components/Navbar/Navbar';
 import { Toaster } from 'react-hot-toast';
 import UserContext from '../lib/context';
@@ -10,6 +11,26 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <UserContext.Provider value={userData}>
+      <Head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
